f39a0ced23d745309cbe8e556d3d11499bd2038d
Jai_Vang <jaivang0387@gmail.com>
2023-11-28 18:04:56 -0500

    adding asyncFileReader.java

---
+++ asyncFileReader.java
@@ -0,0 +1,33 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.CompletableFuture;
+
+public class AsyncFileReader {
+
+    public static void main(String[] args) {
+        String filePath = "sample.txt";
+
+        // Using CompletableFuture for asynchronous execution
+        CompletableFuture.supplyAsync(() -> readFileAsync(filePath))
+                // Handling successful completion
+                .thenAccept(data -> System.out.println("Java Content: " + data))
+                // Handling errors
+                .exceptionally(error -> {
+                    System.err.println("Java Error: " + error.getMessage());
+                    return null;
+                });
+    }
+
+    // Asynchronous file reading method
+    static String readFileAsync(String filePath) {
+        try {
+            // Reading file asynchronously
+            byte[] data = Files.readAllBytes(Paths.get(filePath));
+            return new String(data);
+        } catch (Exception e) {
+            // Throwing a runtime exception for error handling
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
